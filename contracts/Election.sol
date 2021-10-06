93dc25545c62f3d2ff576bdea909acaad674be02
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-10-06 18:20:45 +0530

    DAPP Voting System

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,29 @@
 pragma solidity >=0.4.11;
 contract Election
-{
-	string public candidate;
-	function Elections() public
+{	//Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	//store candidates
+
+	//Fetch candidate
+	mapping(uint => Candidate) public candidates;
+	
+	//store candidates count
+	uint public candidatesCount;
+
+
+	constructor() public
 	{
-		candidate = 'Candidate-1';
+		addCandidate('Candidate-1');
+		addCandidate('Candidate-2');
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
