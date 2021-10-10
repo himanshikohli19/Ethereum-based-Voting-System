f60d968e33c960a8ce00f13d32b79c938ac86c19
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-10-10 20:13:43 +0530

    Voting system project

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,7 +6,8 @@ contract Election
 		string name;
 		uint voteCount;
 	}
-
+	//store accounts that have already voted
+	mapping(address => bool) public voters;
 	//store candidates
 
 	//Fetch candidate
@@ -18,12 +19,26 @@ contract Election
 
 	constructor() public
 	{
-		addCandidate('Candidate-1');
-		addCandidate('Candidate-2');
+		addCandidate('Bhartiya Janta Party (BJP)');
+		addCandidate('Aam Admi Party (AAP)');
 	}
 
 	function addCandidate (string memory _name) private {
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	function vote (uint _candidateId) public {
+		//require that they haven't voted before - trace
+		require(!voters[msg.sender]);
+
+		//require a valid candidate -check
+		require(_candidateId>0 && _candidateId<=candidatesCount);
+
+		//record that voter has voted
+		voters[msg.sender] = true;
+
+		//update candidate vote account
+		candidates[_candidateId].voteCount++;
+
 	}
 }
